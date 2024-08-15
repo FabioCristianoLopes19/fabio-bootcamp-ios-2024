845750602df9989ccde59e78089ff97779e71e90
Fabio Cristiano Lopez <fabiocristianolopesdevios@gmail.com>
2024-08-14 21:46:46 -0300

    Material de estudo.

---
+++ DesafioTransitandoInformacoesEntreTelas/DesafioTransitandoInformacoesEntreTelas/Tela02/Tela02ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  Tela02ViewController.swift
+//  DesafioTransitandoInformacoesEntreTelas
+//
+//  Created by Fabio Cristiano Lopes on 17/07/24.
+//
+
+import UIKit
+
+ 
+    class Tela02ViewController: UIViewController {
+
+        @IBOutlet weak var resultLabel: UILabel!
+        
+      var resultTotal: String
+
+      init?(coder: NSCoder, resultTotal: String) {
+        
+        self.resultTotal = resultTotal
+        super.init(coder: coder)
+      }
+
+      required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+      }
+
+      override func viewDidLoad() {
+        super.viewDidLoad()
+          resultLabel.text = resultTotal.capitalized
+      }
+
+    }
